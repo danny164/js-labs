a67e3f37d08fa266072289f54aae0c6b22357e77
DANNY <qnv164@gmail.com>
2021-07-08 12:12:00 +0700

    update

---
+++ src/06-07-random-a-to-b.js
@@ -0,0 +1,26 @@
+// 2. Random a number in range of [a, b] với a < b
+// min: a
+// range: b - a
+
+// [10, 100]
+// min: 10
+// range: 100 - 10 = 90
+
+function randomNumberInRange(a, b) {
+    if (a >= b) return -1;
+
+    const random = Math.random() * (b - a);
+
+    return Math.round(random) + a;
+}
+
+console.log(randomNumberInRange(10, 99));
+console.log(randomNumberInRange(10, 99));
+console.log(randomNumberInRange(10, 99));
+console.log(randomNumberInRange(100, 999));
+console.log(randomNumberInRange(100, 999));
+console.log(randomNumberInRange(100, 999));
+console.log(randomNumberInRange(1000, 9999));
+console.log(randomNumberInRange(1000, 9999));
+console.log(randomNumberInRange(1000, 9999));
+console.log(randomNumberInRange(1000, 9999));
